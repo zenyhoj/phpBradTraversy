64bdb4d9f0640359d1862739bd701bce3c529761
joe <joe.balingit@gmail.com>
2022-11-29 14:12:43 +0800

    php by brad traversy

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$firstname = 'Joe';
+$lastname = 'Balingit';
+
+// string concatenation
+echo $firstname . ' ' . $lastname;
+
+// defining constant. First parameter is constant name, second is the value
+define('GREETING', 'Hello everyone');
+echo GREETING;
+
+?>
+
