15354b0e30fe38883438c210f1237e5e6659cab7
zenyhoj <joe.balingit@gmail.com>
2022-12-01 21:46:31 +0800

    added sample codes on superglobals and output it using bootstrap

---
+++ website2/index.php
@@ -0,0 +1,64 @@
+
+<?php include 'server_info.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+
+<h1>System and File Info</h1>
+
+<?php if ($server): ?>
+
+    <ul class="list-group">
+
+        <?php foreach($server as $key => $value): ?>
+            <li class="list-group-item">
+
+                <strong><?php echo $key . ':'; ?></strong>
+                <?php echo $value; ?>
+
+            </li>
+        <?php endforeach; ?>
+
+    </ul>
+
+<?php endif; ?>
+
+
+
+<!-- Client Info -->
+<?php if ($client): ?>
+
+<ul class="list-group">
+
+    <?php foreach($client as $key => $value): ?>
+        <li class="list-group-item">
+
+            <strong><?php echo $key . ':'; ?></strong>
+            <?php echo $value; ?>
+
+        </li>
+    <?php endforeach; ?>
+
+</ul>
+
+<?php endif; ?>
+
+
+</div>
+
+
+
+
+
+    
+</body>
+</html>
---
+++ website2/server_info.php
@@ -0,0 +1,23 @@
+<?php
+
+ //SERVER Name
+ $server = [
+ 'Server Name' => $_SERVER['SERVER_NAME'],
+ 'Server Software' => $_SERVER['SERVER_SOFTWARE'],
+ 'Document Root' => $_SERVER['DOCUMENT_ROOT']
+
+];
+
+ //CLIENT INFO
+ $client = [
+
+ 'Client System Info' => $_SERVER['HTTP_USER_AGENT'],
+ 'Client IP Address' => $_SERVER['REMOTE_ADDR'],
+ 'Client Port' => $_SERVER['REMOTE_PORT']
+];
+
+?>
+   
+
+
+   
